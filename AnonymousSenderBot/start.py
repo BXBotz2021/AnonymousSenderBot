da5cca60e820805c3bd934f86b1a28b6c01831c1
Mufaz-TG <93363575+BXBotz2021@users.noreply.github.com>
2024-04-17 16:35:51 +0530

    Update start.py

--- AnonymousSenderBot/start.py
+++ AnonymousSenderBot/start.py
@@ -5,7 +5,7 @@ from pyrogram.types import InlineKeyboardMarkup
 
 # Start Message
 @Client.on_message(filters.private & filters.incoming & filters.command("start"))
-async def start(anonbot, msg):
+async def start(anonbot, message):
     print("/start")
     await anonbot.send_message(
         msg.chat.id,
