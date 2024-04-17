0a66dfc61b96037a527eb02591512975fc5b7e2d
Mufaz-TG <93363575+BXBotz2021@users.noreply.github.com>
2024-04-17 17:22:27 +0530

    Update callbacks.py

--- AnonymousSenderBot/callbacks.py
+++ AnonymousSenderBot/callbacks.py
@@ -7,20 +7,20 @@ from Data import Data
 @Client.on_callback_query()
 async def _calls(anonbot, callback_query):
     chat_id = callback_query.from_user.id
-    message_id = callback_query.message.message_id
+    message.id = callback_query.message.message_id
     if callback_query.data.lower() == "home":
         user = await anonbot.get_me()
         mention = user["mention"]
         await anonbot.edit_message_text(
             chat_id=chat_id,
-            message_id=message_id,
+            message.id=message.id,
             text=Data.START.format(callback_query.from_user.mention, mention),
             reply_markup=InlineKeyboardMarkup(Data.buttons),
         )
     if callback_query.data.lower() == "about":
         await anonbot.edit_message_text(
             chat_id=chat_id,
-            message_id=message_id,
+            message.id=message.id,
             text=Data.ABOUT,
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(Data.home_button),
@@ -28,13 +28,13 @@ async def _calls(anonbot, callback_query):
     if callback_query.data.lower() == "remove":
         caption = ""
         await anonbot.edit_message_caption(
-            chat_id=chat_id, message_id=message_id, caption=caption, reply_markup=InlineKeyboardMarkup([Data.add_button])
+            chat_id=chat_id, message.id=message.id, caption=caption, reply_markup=InlineKeyboardMarkup([Data.add_button])
         )
     if callback_query.data.lower() == "add":
         caption = callback_query.message.reply_to_message.caption
         if caption:
             await anonbot.edit_message_caption(
-                chat_id=chat_id, message_id=message_id, caption=caption, reply_markup=InlineKeyboardMarkup([Data.remove_button])
+                chat_id=chat_id, message.id=message.id, caption=caption, reply_markup=InlineKeyboardMarkup([Data.remove_button])
             )
         else:
             await callback_query.answer("The original message has been deleted or their is no previous caption.", show_alert=True)
